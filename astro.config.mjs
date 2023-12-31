da500bb28ebefac519f79da24920a6388d661682
Islam <islamkwnd@gmail.com>
2023-12-31 16:56:27 +0100

    build: Netlify setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://islami00.github.io/islami00-site',
   integrations: [mdx(), sitemap()],
-  base: '/islami00-site'
+  output: "server",
+  adapter: netlify()
 });
