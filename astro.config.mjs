ddea2677192d6bfff4aedfea14b8d4e7dc915d23
Islam <islamkwnd@gmail.com>
2023-12-31 16:46:34 +0100

    feat(projects): Add deployment details

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://islami00.github.io/islami00-site',
+  integrations: [mdx(), sitemap()],
+  base: '/islami00-site'
 });
