9e8401ed446078b0d27d99f70b0c6b432b1b8e93
Islam <islamkwnd@gmail.com>
2023-12-31 17:11:34 +0100

    fix: FIx env variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import { loadEnv } from 'vite';
+const { DEPLOY_SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: DEPLOY_SITE_URL,
   integrations: [mdx(), sitemap()],
-  output: "server",
+  output: 'server',
   adapter: netlify()
 });
